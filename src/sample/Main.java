b265f10d9c90932da3cebaf673ce1d9906b75dd5
Irfan85 <irfan.muhit85@gmail.com>
2022-05-02 14:28:45 +0600

    Adding nodes to the scene using fxml file

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -16,17 +16,7 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-//		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-		
-		GridPane root = new GridPane();
-		root.setAlignment(Pos.CENTER);
-		root.setVgap(10);
-		root.setHgap(10);
-		
-		Label greeting = new Label("Welcome to JavaFX!");
-		greeting.setTextFill(Color.GREEN);
-		greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 18));
-		root.getChildren().add(greeting);
+		Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 		
 		primaryStage.setTitle("Hello JavaFX!");
 		primaryStage.setScene(new Scene(root, 640, 480));
